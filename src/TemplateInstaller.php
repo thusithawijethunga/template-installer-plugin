33c290a9458570752d45ea0a35a6f8cb60c26e4c
Thusitha Avinda <thusithawijethunga@gmail.com>
2023-03-03 03:19:47 +0530

    WordPress download

--- src/TemplateInstaller.php
+++ src/TemplateInstaller.php
@@ -4,6 +4,7 @@ namespace Thusitha;
 
 use Composer\Package\PackageInterface;
 use Composer\Installer\LibraryInstaller;
+use ZipArchive;
 
 class TemplateInstaller extends LibraryInstaller
 {
@@ -22,7 +23,7 @@ class TemplateInstaller extends LibraryInstaller
         }
 
         $location = 'src/Modules/' . ucfirst(substr($package->getPrettyName(), 17));
- 
+
         return $location;
     }
 
@@ -32,5 +33,25 @@ class TemplateInstaller extends LibraryInstaller
     public function supports($packageType)
     {
         return 'laravel-plugin' === $packageType;
+    }
+
+    public function qdownload(string $packageName, string $version, string $installPath): void
+    {
+        // Construct the URL for the WordPress plugin ZIP file.
+        //$url = sprintf('https://downloads.wordpress.org/plugin/%1$s.%2$s.zip', $packageName, $version);
+        //echo $url;
+
+        // Download the ZIP file.
+        // $tempFile = tempnam(sys_get_temp_dir(), 'wp_plugin');
+        // file_put_contents($tempFile, file_get_contents($url));
+
+        // // Unzip the ZIP file to the installation path.
+        // $zip = new ZipArchive();
+        // $zip->open($tempFile);
+        // $zip->extractTo($installPath);
+        // $zip->close();
+
+        // // Remove the temporary ZIP file.
+        // unlink($tempFile);
     }
 }
--- src/TemplateInstallerPlugin.php
+++ src/TemplateInstallerPlugin.php
@@ -13,9 +13,17 @@ class TemplateInstallerPlugin implements PluginInterface
         echo '...................................................' . PHP_EOL;
         echo 'Hi, I am Installer' . PHP_EOL;
         echo '...................................................' . PHP_EOL;
-        
+
         $installer = new TemplateInstaller($io, $composer);
         $composer->getInstallationManager()->addInstaller($installer);
+
+        // set the package name of the WordPress plugin to download
+        //$packageName = 'w3-total-cache';
+        // set the version of the WordPress plugin to download
+        //$version = '0.9.7.5';
+        // set the installation path for the downloaded WordPress plugin
+        // $installPath = 'vendor/wp-plugins/w3-total-cache';
+        //$installer->qdownload($packageName, $version, $installPath);
     }
 
     public function deactivate(Composer $composer, IOInterface $io)
