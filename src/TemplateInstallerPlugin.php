c296cdb803220518c5027589973cf2f52844426b
Thusitha Avinda <thusithawijethunga@gmail.com>
2023-03-03 03:13:35 +0530

    init code

---
+++ src/TemplateInstallerPlugin.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Thusitha;
+
+use Composer\Composer;
+use Composer\IO\IOInterface;
+use Composer\Plugin\PluginInterface;
+
+class TemplateInstallerPlugin implements PluginInterface
+{
+    public function activate(Composer $composer, IOInterface $io)
+    {
+        echo '...................................................' . PHP_EOL;
+        echo 'Hi, I am Installer' . PHP_EOL;
+        echo '...................................................' . PHP_EOL;
+        
+        $installer = new TemplateInstaller($io, $composer);
+        $composer->getInstallationManager()->addInstaller($installer);
+    }
+
+    public function deactivate(Composer $composer, IOInterface $io)
+    {
+    }
+
+    public function uninstall(Composer $composer, IOInterface $io)
+    {
+    }
+}
